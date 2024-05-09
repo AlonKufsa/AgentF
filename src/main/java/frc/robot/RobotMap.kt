39984f01b3fbdc1fd649a8be5b5581d4557120c6
Alon <alon.gal.kufsa@gmail.com>
2024-05-09 20:14:06 +0300

    Shaked renamed constants

--- src/main/java/frc/robot/RobotMap.kt
+++ src/main/java/frc/robot/RobotMap.kt
@@ -3,18 +3,20 @@ package frc.robot
 object RobotMap {
 	object ShooterMap {
 		const val SHOOTER_ANGLE_MOTOR_ID = 4
-		const val SHOOTER_TOP_SHOOTING_MOTOR_ID = 0
-		const val SHOOTER_BOTTOM_SHOOTING_MOTOR_ID = 0
+		const val SHOOTER_TOP_SHOOTING_MOTOR_ID = 23
+		const val SHOOTER_BOTTOM_SHOOTING_MOTOR_ID = 24
 		const val CANCODER_ID = 3
-		const val MIN_SWITCH_ID = 0
-		const val MAX_SWITCH_ID = 1
+		const val MIN_SWITCH_CHANNEL = 0
+		const val MAX_SWITCH_CHANNEL = 1
 	}
 
 	object IntakeMap {
-
+		const val BOTTOM_MOTOR_ID: Int = 20
+		const val TOP_MOTOR_ID: Int = 21
 	}
 
 	object LoaderMap {
-
+		const val MOTOR_ID = 22
+		const val BEAMBREAK_CHANNEL = 6
 	}
 }
