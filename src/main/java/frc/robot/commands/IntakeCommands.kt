fa434cf47291244fda1b72cfc7e93e88c3de5dc5
Alon <alon.gal.kufsa@gmail.com>
2024-05-16 15:35:16 +0300

    Made intake not run if shooter is not within angle tolerance to collect

--- src/main/java/frc/robot/commands/IntakeCommands.kt
+++ src/main/java/frc/robot/commands/IntakeCommands.kt
@@ -2,6 +2,8 @@ package frc.robot.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.shooter.ShooterState
+import frc.robot.subsystems.shooter.ShooterSubsystem
 
 class DefaultIntakeCommand : Command() {
 	init {
@@ -22,7 +24,11 @@ class RunIntakeCommand : Command() {
 	}
 
 	override fun execute() {
-		IntakeSubsystem.runMotors()
+		if (ShooterSubsystem.isWithinAngleTolerance && ShooterSubsystem.angleSetpoint == ShooterState.COLLECT.angle) {
+			IntakeSubsystem.runMotors()
+		} else {
+			IntakeSubsystem.stopMotors()
+		}
 	}
 
 	override fun end(interrupted: Boolean) {
