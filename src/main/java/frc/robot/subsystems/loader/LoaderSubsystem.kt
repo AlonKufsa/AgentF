98ebe463ef3093d7fbd38733e8d1cab28091f9cc
Alon <alon.gal.kufsa@gmail.com>
2024-05-20 15:49:25 +0300

    Added dashboard info for the loader subsystem

--- src/main/java/frc/robot/subsystems/loader/LoaderSubsystem.kt
+++ src/main/java/frc/robot/subsystems/loader/LoaderSubsystem.kt
@@ -2,9 +2,11 @@ package frc.robot.subsystems.loader
 
 import com.ctre.phoenix6.hardware.TalonFX
 import com.hamosad1657.lib.units.Volts
+import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.wpilibj.AnalogInput
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
+import kotlin.math.absoluteValue
 
 object LoaderSubsystem : SubsystemBase("Loader") {
 	private val motor = TalonFX(RobotMap.LoaderMap.MOTOR_ID)
@@ -19,5 +21,11 @@ object LoaderSubsystem : SubsystemBase("Loader") {
 
 	fun stopMotor() {
 		motor.stopMotor()
+	}
+
+	override fun initSendable(builder: SendableBuilder) {
+		builder.addBooleanProperty("isNoteDetected", { isNoteDetected }, null)
+		builder.addBooleanProperty("isMotorRunning", { motor.get().absoluteValue > 0.0 }, null)
+		builder.addDoubleProperty("Beambreak voltage", { beamBreak.voltage }, null)
 	}
 }
