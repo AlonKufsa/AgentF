816272722e3f0b07487d92bc13cfc787373738f8
AlonKufsa <alon.gal.kufsa@gmail.com>
2024-06-20 19:08:56 +0300

    Alon: added configs to motors and cancoder and made the SwerveModule sendable to the builder

--- src/main/java/frc/robot/subsystems/swerve/SwerveConstants.kt
+++ src/main/java/frc/robot/subsystems/swerve/SwerveConstants.kt
@@ -1,21 +1,32 @@
 package frc.robot.subsystems.swerve
 
-import com.ctre.phoenix6.configs.FeedbackConfigs
+import com.ctre.phoenix6.configs.CANcoderConfiguration
 import com.ctre.phoenix6.configs.TalonFXConfiguration
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue.Unsigned_0To1
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue.RemoteCANcoder
+import com.ctre.phoenix6.signals.SensorDirectionValue.CounterClockwise_Positive
 import com.hamosad1657.lib.math.PIDGains
 import com.hamosad1657.lib.units.meters
 import com.hamosad1657.lib.units.rps
-import kotlin.math.PI
+import edu.wpi.first.wpilibj.DriverStation
 
 object SwerveConstants {
+	//Canbus network swerve is connected to
 	const val SWERVE_CANBUS = "SwerveBus"
 
+	//Drive pid and ff values
 	val DRIVE_PID_GAINS = PIDGains()
+	const val DRIVE_KA = 0.0
+	const val DRIVE_KV = 0.0
+	const val DRIVE_KS = 0.0
+
+	//Steer pid and ff values
 	val STEER_PID_GAINS = PIDGains()
+	const val STEER_KA = 0.0
+	const val STEER_KV = 0.0
+	const val STEER_KS = 0.0
 
 	const val WHEEL_CIRCUMFERENCE_METERS = 0.0
-	const val WHEEL_RADIUS_METERS = WHEEL_CIRCUMFERENCE_METERS / (2 * PI)
 
 	// Theoretical free speed (m/s) at 12v applied output.
 	const val MAX_SPEED_MPS = 9.0 // 9.46 according to CTRE ?
@@ -26,28 +37,67 @@ object SwerveConstants {
 	// The distance from the center of the chassis to a center of a module.
 	private val DRIVEBASE_RADIUS = 0.417405.meters
 
-	const val TOP_RIGHT_OFFSET_DEG = 0.0
-	const val TOP_LEFT_OFFSET_DEG = 0.0
+	const val FRONT_RIGHT_OFFSET_DEG = 0.0
+	const val FRONT_LEFT_OFFSET_DEG = 0.0
 	const val BOTTOM_LEFT_OFFSET_DEG = 0.0
 	const val BOTTOM_RIGHT_OFFSET_DEG = 0.0
 
 	val DRIVE_MOTOR_CONFIGS: TalonFXConfiguration = TalonFXConfiguration().apply {
+		//Current limits
 		CurrentLimits.SupplyCurrentLimit = 45.0
 		CurrentLimits.SupplyCurrentLimitEnable = true
 		CurrentLimits.StatorCurrentLimitEnable = true
 
+		//Limits the speed in which the motors voltage consumption can change
 		ClosedLoopRamps.VoltageClosedLoopRampPeriod = 0.25
+
+		//PID and FF
+		with(Slot0) {
+			//PID
+			kP = DRIVE_PID_GAINS.kP
+			kI = DRIVE_PID_GAINS.kI
+			kD = DRIVE_PID_GAINS.kD
+
+			//FF
+			kA = DRIVE_KA
+			kV = DRIVE_KV
+			kS = DRIVE_KS
+		}
 	}
 
-	fun steerMotorFeedbackConfigs(canCoderID: Int, moduleName: String): FeedbackConfigs = FeedbackConfigs().apply {
-		FeedbackRemoteSensorID = canCoderID
-		FeedbackSensorSource = RemoteCANcoder
-		FeedbackRotorOffset = when (moduleName) {
-			"TopRight" -> TOP_RIGHT_OFFSET_DEG
-			"TopLeft" -> TOP_LEFT_OFFSET_DEG
-			"BottomLeft" -> BOTTOM_LEFT_OFFSET_DEG
-			"BottomRight" -> BOTTOM_RIGHT_OFFSET_DEG
-			else -> 0.0
+	fun steerMotorConfigs(canCoderID: Int, moduleName: String): TalonFXConfiguration = TalonFXConfiguration().apply {
+		CurrentLimits.SupplyCurrentLimit = 20.0
+		CurrentLimits.StatorCurrentLimitEnable = true
+
+		Feedback.FeedbackRemoteSensorID = canCoderID
+		Feedback.FeedbackSensorSource = RemoteCANcoder
+
+		with(Slot0) {
+			//PID
+			kP = STEER_PID_GAINS.kP
+			kI = STEER_PID_GAINS.kI
+			kD = STEER_PID_GAINS.kD
+
+			//FF
+			kA = STEER_KA
+			kV = STEER_KV
+			kS = STEER_KS
+		}
+	}
+
+	fun canCoderConfigs(moduleName: String): CANcoderConfiguration = CANcoderConfiguration().apply {
+		with(MagnetSensor) {
+			//The offset added to the CANCoder for it to measure correctly for a wheel pointing right to be 0 degrees
+			MagnetOffset = when (moduleName) {
+				"FrontRight" -> FRONT_RIGHT_OFFSET_DEG
+				"FrontLeft" -> FRONT_LEFT_OFFSET_DEG
+				"BottomLeft" -> BOTTOM_LEFT_OFFSET_DEG
+				"BottomRight" -> BOTTOM_RIGHT_OFFSET_DEG
+				else -> 0.0.also { DriverStation.reportError("Illegal swerve module name: $moduleName", false) }
+			}
+
+			SensorDirection = CounterClockwise_Positive
+			AbsoluteSensorRange = Unsigned_0To1
 		}
 	}
 
--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.kt
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.kt
@@ -6,43 +6,42 @@ import com.hamosad1657.lib.units.AngularVelocity
 import com.revrobotics.CANSparkBase.IdleMode
 import com.revrobotics.CANSparkBase.IdleMode.kBrake
 import edu.wpi.first.math.geometry.Rotation2d
-import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.kinematics.SwerveModuleState
+import edu.wpi.first.util.sendable.Sendable
+import edu.wpi.first.util.sendable.SendableBuilder
 import frc.robot.subsystems.swerve.SwerveConstants as Constants
 
 class SwerveModule(
-	val posFromCenterMeters: Translation2d,
 	private val driveMotorID: Int,
 	private val steerMotorID: Int,
 	private val canCoderID: Int,
-	private val canCoderOffset: Double,
 	val moduleName: String,
-) {
+) : Sendable {
 
 	//Motor for controlling the velocity of the wheel
 	val driveMotor = HaTalonFX(driveMotorID, Constants.SWERVE_CANBUS).apply {
 		restoreFactoryDefaults()
-		configPID(Constants.DRIVE_PID_GAINS)
-		
+		configurator.apply(Constants.DRIVE_MOTOR_CONFIGS)
 	}
 
 	//Motor for controlling the angle of the wheel
 	val steerMotor = HaTalonFX(steerMotorID, Constants.SWERVE_CANBUS).apply {
 		restoreFactoryDefaults()
-		configurator.apply(Constants.steerMotorFeedbackConfigs(canCoderID, moduleName))
-		configPID(Constants.STEER_PID_GAINS)
+		configurator.apply(Constants.steerMotorConfigs(canCoderID, moduleName))
 	}
 
 
 	val canCoder = CANcoder(canCoderID, Constants.SWERVE_CANBUS)
 
 
-	//Make the user able to change the idle mode of the motors in the module. Brake by default
+	/** Change this to change the driving motor's idle state. kBrake by default */
 	var driveIdleMode: IdleMode = kBrake
 		set(value) {
 			driveMotor.idleMode = value
 			field = value
 		}
+
+	/** Change this to change the steering motor's idle state. kBrake by default */
 	var steerIdleMode: IdleMode = kBrake
 		set(value) {
 			steerMotor.idleMode = value
@@ -51,16 +50,37 @@ class SwerveModule(
 
 
 	//The angle setpoint of the swerve module from 0.0 to 360.0 from the right side of the x axis counter clockwise
-	private var angleSetpointDeg: Rotation2d = Rotation2d(0.0)
+	private var angleSetpoint: Rotation2d = Rotation2d(0.0)
 
 	//The angular velocity setpoint of the wheel in rps
-	private var wheelAngularVelocitySetpointRPS: AngularVelocity = AngularVelocity.fromRps(0.0)
+	private var wheelAngularVelocitySetpoint: AngularVelocity = AngularVelocity.fromRps(0.0)
 
+	/** Function for setting the module state externally */
 	fun setModuleState(swerveModuleState: SwerveModuleState) {
-		angleSetpointDeg = swerveModuleState.angle
-		wheelAngularVelocitySetpointRPS = AngularVelocity.fromRps(
+		//TODO: continuous wrap the angle
+		angleSetpoint = swerveModuleState.angle
+		wheelAngularVelocitySetpoint = AngularVelocity.fromRps(
 			swerveModuleState.speedMetersPerSecond / Constants.WHEEL_CIRCUMFERENCE_METERS)
+
+
 	}
 
 
+	//Shortcuts for getting speed and rotation, and ways of accessing them outside of the class
+	val moduleRotationDeg: Double
+		get() = canCoder.position.value * 360.0
+	val moduleSpeedMPS: Double
+		get() = driveMotor.velocity.value * Constants.WHEEL_CIRCUMFERENCE_METERS
+
+
+	//Logging
+	override fun initSendable(builder: SendableBuilder) {
+		builder.addDoubleProperty("$moduleName rotation deg", { moduleRotationDeg }, null)
+		builder.addDoubleProperty("$moduleName speed MPS", { moduleSpeedMPS }, null)
+
+		builder.addDoubleProperty("$moduleName rotation setpoint deg", { angleSetpoint.degrees }, null)
+		builder.addDoubleProperty("$moduleName speed setpoint MPS",
+			{ wheelAngularVelocitySetpoint.asRps * Constants.WHEEL_CIRCUMFERENCE_METERS },
+			null)
+	}
 }
