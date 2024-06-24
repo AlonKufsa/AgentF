281e175404d89307f719e17aad50fd82957edb39
AlonKufsa <alon.gal.kufsa@gmail.com>
2024-06-24 14:03:43 +0300

    Added modules for SwerveSubsystem

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
@@ -1,8 +1,33 @@
 package frc.robot.subsystems.swerve
 
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.RobotMap.SwerveMap
 
 object SwerveSubsystem : SubsystemBase("Swerve subsystem") {
+	val frontRight = SwerveModule(
+		SwerveMap.FrontRight.DRIVE_MOTOR_ID,
+		SwerveMap.FrontRight.STEER_MOTOR_ID,
+		SwerveMap.FrontRight.CANCODER_ID,
+		"FrontRight"
+	)
+	val frontLeft = SwerveModule(
+		SwerveMap.FrontLeft.DRIVE_MOTOR_ID,
+		SwerveMap.FrontLeft.STEER_MOTOR_ID,
+		SwerveMap.FrontLeft.CANCODER_ID,
+		"FrontRight"
+	)
+	val backLeft = SwerveModule(
+		SwerveMap.BackLeft.DRIVE_MOTOR_ID,
+		SwerveMap.BackLeft.STEER_MOTOR_ID,
+		SwerveMap.BackLeft.CANCODER_ID,
+		"FrontRight"
+	)
+	val backRight = SwerveModule(
+		SwerveMap.BackRight.DRIVE_MOTOR_ID,
+		SwerveMap.BackRight.STEER_MOTOR_ID,
+		SwerveMap.BackRight.CANCODER_ID,
+		"FrontRight"
+	)
+
 
 }
