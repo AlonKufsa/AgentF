70d99d4bc288d5ba233781fdd5f85077431a9ceb
AlonKufsa <alon.gal.kufsa@gmail.com>
2024-06-24 15:04:48 +0300

    Clarified that the functions are robot relative

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveKinematics.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveKinematics.kt
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.swerve
 
 import com.hamosad1657.lib.units.AngularVelocity
-import com.hamosad1657.lib.units.AngularVelocity.Companion
 import edu.wpi.first.math.MathUtil
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
@@ -11,8 +10,11 @@ import kotlin.math.PI
 import frc.robot.subsystems.swerve.SwerveConstants as Constants
 
 object SwerveKinematics {
-	/** Converts between angular velocity of the robot to the module states needed to achieve it.
-	 * Positive angular velocity is counterclockwise, negative is clockwise */
+
+	/**
+	 * Converts between angular velocity of the robot to the module states needed to achieve it.
+	 * Positive angular velocity is counterclockwise, negative is clockwise.
+	 */
 	fun angularVelocityToModuleStates(angularVelocity: AngularVelocity): ModuleStates {
 		val wheelSpeedMPS: Double = angularVelocity.asRps * 2 * PI * Constants.DRIVEBASE_RADIUS.asMeters
 
@@ -22,28 +24,28 @@ object SwerveKinematics {
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(315.0)),
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(45.0)),
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(135.0)),
-				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(225.0))
+				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(225.0)),
 			)
 		} else {
 			moduleStates.setStates(
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(135.0)),
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(225.0)),
 				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(315.0)),
-				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(45.0))
+				SwerveModuleState(wheelSpeedMPS, Rotation2d.fromDegrees(45.0)),
 			)
 		}
 		return moduleStates
 	}
 
 	/** Converts between a velocity in some direction to the module states needed to achieve it. */
-	fun velocityToModuleStates(velocity: Translation2d): ModuleStates {
-		//The Translation2d.angle sometimes gives negative angles.
+	fun robotRelativeVelocityToModuleStates(velocity: Translation2d): ModuleStates {
+		// The Translation2d.angle sometimes gives negative angles.
 		val modulatedAngle = Rotation2d.fromDegrees(MathUtil.inputModulus(velocity.angle.degrees, 0.0, 360.0))
 		return ModuleStates(
 			SwerveModuleState(velocity.norm, modulatedAngle),
 			SwerveModuleState(velocity.norm, modulatedAngle),
 			SwerveModuleState(velocity.norm, modulatedAngle),
-			SwerveModuleState(velocity.norm, modulatedAngle)
+			SwerveModuleState(velocity.norm, modulatedAngle),
 		)
 	}
 
@@ -53,10 +55,10 @@ object SwerveKinematics {
 
 	/** Converts between chassis speeds and module states.
 	 * Positive chassis speeds omega results in counterclockwise rotation.*/
-	fun chassisSpeedsToModuleStates(chassisSpeeds: ChassisSpeeds): ModuleStates {
+	fun robotRelativeChassisSpeedsToModuleStates(chassisSpeeds: ChassisSpeeds): ModuleStates {
 		val velocity = Translation2d(chassisSpeeds.vxMetersPerSecond, chassisSpeeds.vyMetersPerSecond)
 
-		val velocityModuleStates = velocityToModuleStates(velocity)
+		val velocityModuleStates = robotRelativeVelocityToModuleStates(velocity)
 		val rotationModuleStates = angularVelocityToModuleStates(AngularVelocity.fromRadPs(chassisSpeeds.omegaRadiansPerSecond))
 
 		val frontRightCombined: Translation2d =
@@ -72,9 +74,7 @@ object SwerveKinematics {
 			SwerveModuleState(frontRightCombined.norm, frontRightCombined.angle),
 			SwerveModuleState(frontLeftCombined.norm, frontLeftCombined.angle),
 			SwerveModuleState(backLeftCombined.norm, backLeftCombined.angle),
-			SwerveModuleState(backRightCombined.norm, backRightCombined.angle)
+			SwerveModuleState(backRightCombined.norm, backRightCombined.angle),
 		)
 	}
-
-
 }
