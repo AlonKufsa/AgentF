a0b66dce5ba1bdc92a5cd10920ada390ad126164
AlonKufsa <alon.gal.kufsa@gmail.com>
2024-06-24 15:03:18 +0300

    Added relativeDrive function

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
@@ -1,30 +1,31 @@
 package frc.robot.subsystems.swerve
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap.SwerveMap
 
 object SwerveSubsystem : SubsystemBase("Swerve subsystem") {
-	val frontRight = SwerveModule(
+	private val frontRight = SwerveModule(
 		SwerveMap.FrontRight.DRIVE_MOTOR_ID,
 		SwerveMap.FrontRight.STEER_MOTOR_ID,
 		SwerveMap.FrontRight.CANCODER_ID,
 		"FrontRight"
 	)
-	val frontLeft = SwerveModule(
+	private val frontLeft = SwerveModule(
 		SwerveMap.FrontLeft.DRIVE_MOTOR_ID,
 		SwerveMap.FrontLeft.STEER_MOTOR_ID,
 		SwerveMap.FrontLeft.CANCODER_ID,
 		"FrontRight"
 	)
-	val backLeft = SwerveModule(
+	private val backLeft = SwerveModule(
 		SwerveMap.BackLeft.DRIVE_MOTOR_ID,
 		SwerveMap.BackLeft.STEER_MOTOR_ID,
 		SwerveMap.BackLeft.CANCODER_ID,
 		"FrontRight"
 	)
-	val backRight = SwerveModule(
+	private val backRight = SwerveModule(
 		SwerveMap.BackRight.DRIVE_MOTOR_ID,
 		SwerveMap.BackRight.STEER_MOTOR_ID,
 		SwerveMap.BackRight.CANCODER_ID,
@@ -37,6 +38,13 @@ object SwerveSubsystem : SubsystemBase("Swerve subsystem") {
 		backLeft.setModuleState(moduleStates.backLeft)
 		backRight.setModuleState(moduleStates.backRight)
 	}
+
+	fun robotRelativeDrive(chassisSpeeds: ChassisSpeeds) {
+		val moduleStates = SwerveKinematics.robotRelativeChassisSpeedsToModuleStates(chassisSpeeds)
+
+		setModuleStates(moduleStates)
+	}
+
 
 	override fun initSendable(builder: SendableBuilder?) {
 		SmartDashboard.putData(frontRight)
