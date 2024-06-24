f4fc0dd0e292d5eb944e4dee9d8c65df73ab6963
AlonKufsa <alon.gal.kufsa@gmail.com>
2024-06-24 13:00:52 +0300

    Removed unnecessary function from swerve subsystem

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveSubsystem.kt
@@ -4,10 +4,5 @@ import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 object SwerveSubsystem : SubsystemBase("Swerve subsystem") {
-	//Converts from WPILib's 0 to 180 and then -180 to 0 with the positive side of the y axis rotation system
-	//to the standard 0 to 360 with the positive side of the x axis rotation system used with vectors
-	private fun convertRotationFromWPILibToStandard(angle: Rotation2d): Rotation2d =
-		Rotation2d.fromDegrees((450.0 + angle.degrees) % 360)
-
 
 }
