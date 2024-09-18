0228df3fbc24945eb6368e6a093b992cb5023de0
Alon <alon.gal.kufsa@gmail.com>
2024-09-18 19:33:00 +0300

    started work on robot vision (photonlib version doesnt match pi's version)

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -11,6 +11,7 @@ import frc.robot.subsystems.loader.LoaderSubsystem
 import frc.robot.subsystems.shooter.ShooterState
 import frc.robot.subsystems.shooter.ShooterSubsystem
 import frc.robot.subsystems.swerve.SwerveSubsystem
+import frc.robot.subsystems.vision.noteVision
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -66,6 +67,7 @@ object RobotContainer {
 		SmartDashboard.putData(IntakeSubsystem)
 		SmartDashboard.putData(LedSubsystem)
 		SmartDashboard.putData(SwerveSubsystem)
+		SmartDashboard.putData(noteVision)
 	}
 
 	fun getAutonomousCommand(): Command? {
---
+++ src/main/kotlin/frc/robot/subsystems/vision/noteVision.kt
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.vision
+
+import edu.wpi.first.util.sendable.SendableBuilder
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.photonvision.PhotonCamera
+import org.photonvision.targeting.PhotonPipelineResult
+
+object noteVision : SubsystemBase() {
+
+	val camera: PhotonCamera = PhotonCamera("NOTE-Cam")
+
+	val lastResult: PhotonPipelineResult
+		get() = camera.latestResult
+
+
+	override fun initSendable(builder: SendableBuilder) {
+		builder.addBooleanProperty("Is note detected", { lastResult.hasTargets() }, null)
+	}
+}
