ca9cb36f35747a7337841cd94cf8fff9aea43ed8
Alon <alon.gal.kufsa@gmail.com>
2024-09-19 19:54:08 +0300

    Changes to NoteVision

--- src/main/kotlin/frc/robot/subsystems/vision/NoteVision.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/NoteVision.kt
@@ -3,17 +3,18 @@ package frc.robot.subsystems.vision
 import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.photonvision.PhotonCamera
-import org.photonvision.targeting.PhotonPipelineResult
 
 object NoteVision : SubsystemBase("Note vision") {
 
-	val camera: PhotonCamera = PhotonCamera("NOTE-Cam")
-
-	val lastResult: PhotonPipelineResult
+	private val camera: PhotonCamera = PhotonCamera("NOTE-Cam")
+	val lastResult
 		get() = camera.latestResult
+	val hasTargets = lastResult.hasTargets()
 
 
 	override fun initSendable(builder: SendableBuilder) {
-		builder.addBooleanProperty("Is note detected", { lastResult.hasTargets() }, null)
+		builder.addDoubleProperty("note translation",
+			{ if (lastResult != null) lastResult.bestTarget.yaw else 0.0 },
+			null)
 	}
 }
