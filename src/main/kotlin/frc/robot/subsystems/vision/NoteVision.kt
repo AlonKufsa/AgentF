129a6c64fc3c422a85747c2ae7e8993121b25c58
Alon <alon.gal.kufsa@gmail.com>
2024-09-24 14:26:59 +0300

    Fixed NoteVision as sendable not showing data

--- src/main/kotlin/frc/robot/subsystems/vision/NoteVision.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/NoteVision.kt
@@ -1,11 +1,10 @@
 package frc.robot.subsystems.vision
 
+import edu.wpi.first.util.sendable.Sendable
 import edu.wpi.first.util.sendable.SendableBuilder
-import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.photonvision.PhotonCamera
 
-object NoteVision : SubsystemBase("Note vision") {
-
+object NoteVision : Sendable {
 	private val camera: PhotonCamera = PhotonCamera("NOTE-Cam")
 	val lastResult
 		get() = camera.latestResult
@@ -13,6 +12,8 @@ object NoteVision : SubsystemBase("Note vision") {
 
 
 	override fun initSendable(builder: SendableBuilder) {
+		builder.setSmartDashboardType("NoteVision")
+		builder.addBooleanProperty("Has targets", { hasTargets }, null)
 		builder.addDoubleProperty("note translation",
 			{ if (lastResult != null) lastResult.bestTarget.yaw else 0.0 },
 			null)
